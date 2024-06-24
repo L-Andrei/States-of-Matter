72e3bebdc92a4d26b336dc97ae6d786bfebb0aa3
L-Andrei <lucas23marin@gmail.com>
2024-06-24 14:43:33 -0300

    versão final

--- programa/main.cpp
+++ programa/main.cpp
@@ -38,7 +38,64 @@ void executaTeste(double volume1, double volume2, double volume3, double aquecim
     std::cout << "Água demorou: " << loops_agua << " segundos" << std::endl;
     std::cout << "Etanol demorou: " << loops_etanol << " segundos" << std::endl;
     std::cout << "Benzeno demorou: " << loops_benzeno << " segundos" << std::endl;
+    std::cout << "\n" << std::endl;
+}
 
+void compara(double v1, double v2, double v3, double aquecimento){
+    bool sair = true;
+    Ambiente amb = Ambiente(1,0); // ambiente a nível do mar.
+    Substancia agua= Substancia(v1,1,0,4184);
+    Substancia benzeno = Substancia(v2,0.879,0,1720);
+    Substancia etanol = Substancia(v3,0.789,0,2.44);
+
+    // Calcula os pontos de ebulição de cada substância.
+    agua.calculaPontoDeEbulicao(8.07131, 1730.63, 233.426, amb.conversor());
+    benzeno.calculaPontoDeEbulicao(6.90565, 1211.033, 220.790, amb.conversor());
+    etanol.calculaPontoDeEbulicao(8.20417, 1642.89, 230.300, amb.conversor());
+
+    while(sair){
+
+        agua.esquentar(aquecimento);
+        benzeno.esquentar(aquecimento);
+        etanol.esquentar(aquecimento);
+
+        if(agua.entrouEmEbulicao() && benzeno.entrouEmEbulicao() && etanol.entrouEmEbulicao()){
+
+            sair = false;
+
+        }else if(agua.entrouEmEbulicao()){
+
+            agua= Substancia(v1+=1,1,0,4184);
+            benzeno = Substancia(v2,0.879,0,1720);
+            etanol = Substancia(v3,0.789,0,2.44);
+            agua.calculaPontoDeEbulicao(8.07131, 1730.63, 233.426, amb.conversor());
+            benzeno.calculaPontoDeEbulicao(6.90565, 1211.033, 220.790, amb.conversor());
+            etanol.calculaPontoDeEbulicao(8.20417, 1642.89, 230.300, amb.conversor());
+
+        }else if(benzeno.entrouEmEbulicao()){
+
+            agua= Substancia(v1,1,0,4184);
+            benzeno = Substancia(v2+=1,0.879,0,1720);
+            etanol = Substancia(v3,0.789,0,2.44);
+            agua.calculaPontoDeEbulicao(8.07131, 1730.63, 233.426, amb.conversor());
+            benzeno.calculaPontoDeEbulicao(6.90565, 1211.033, 220.790, amb.conversor());
+            etanol.calculaPontoDeEbulicao(8.20417, 1642.89, 230.300, amb.conversor());
+
+        }else if(etanol.entrouEmEbulicao()){
+            agua= Substancia(v1,1,0,4184);
+            benzeno = Substancia(v2,0.879,0,1720);
+            etanol = Substancia(v3+=100,0.789,0,2.44);
+            agua.calculaPontoDeEbulicao(8.07131, 1730.63, 233.426, amb.conversor());
+            benzeno.calculaPontoDeEbulicao(6.90565, 1211.033, 220.790, amb.conversor());
+            etanol.calculaPontoDeEbulicao(8.20417, 1642.89, 230.300, amb.conversor());
+        }
+
+    }
+
+    std::cout << "Para que o benzeno é a água possuam o mesmo tempo de ebolição a volume de cada um deverá ser:" << std::endl;
+    std::cout << "Benzeno " << v2 << " ml" << std::endl;
+    std::cout << "Água " << v1 << " ml" << std::endl;
+    std::cout << "\n" << std::endl;
 }
 
 
@@ -50,7 +107,13 @@ int main() {
     volume2 = 345.5;
     volume3 = 277500;
     
-    executaTeste(volume1,volume2,volume3,aquecimento);
-
+    //executaTeste(volume1,volume2,volume3,aquecimento);
+    /*
+    for(int i=1000; i<100000;i+=10000){
+        executaTeste(volume1,volume2,volume3,i);
+    }
+    */
+    compara(100,100,10000,1000);
+    executaTeste(299,1033,1,10000);
     return 0;
 }
